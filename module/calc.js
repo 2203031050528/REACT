53fb8977c6444fe603b8854edb3aad2059803b39
Rahul Jangir <rahuljangir4368@gmail.com>
2024-12-14 20:19:44 +0530

    module in react

---
+++ module/calc.js
@@ -0,0 +1,22 @@
+// let calc= (a,b)=>{
+//     return a+b;
+
+// }
+
+// let subt= (a,b)=>{
+//     return a-b;
+
+// }
+// export  {calc,subt}
+
+
+
+c= (a,b)=>{
+    return a+b;
+
+}
+
+let subt= (a,b)=>{
+    return a-b;
+
+}
---
+++ module/index.js
@@ -0,0 +1,5 @@
+import {calc,subt} from "./calc.js";
+
+
+console.log(calc(5,8))
+console.log(subt(10,5))
