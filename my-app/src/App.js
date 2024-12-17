ea4ae9bb844279461f657219ae2de3af9040473a
Rahul Jangir <rahuljangir4368@gmail.com>
2024-12-17 22:52:49 +0530

    Add some new components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,94 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Footer from "./Footer";
+import Header from "./header";
+import {Button, Container, Row, Col,Card } from "react-bootstrap";
 
 function App() {
-  let name = "Rahul Jangir"
-  let arr = [56,82,25,28]
-  let status = true
   return (
-    <div className="App">
-     <h1 style={{color:"pink"}}> hii  this is app </h1>
-     <h2> My name is {name} </h2>
-     <h3> My array elements are {arr} </h3>
-     {
-      arr.map((v)=>{
-        return <p key={v}>{v}</p>
-        
-      })
-     }
-       {
-      (status)? <h1>welcome</h1>
-      : ""
-     }
+    <div className="main">
+      <Header />
+
+      <Container>
+        <Container>
+          <Row>
+            <Col className="col-12" text-center py-4>
+              
+              <h1>Welcome to WS </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="3" md-6>
+              <Card style={{ width: "18rem" }}>
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+              
+            </Col>
+
+            <Col lg="3" md-6>
+              <Card style={{ width: "18rem" }}>
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+              
+            </Col>
+            <Col lg="3" md-6>
+              <Card style={{ width: "18rem" }}>
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+              
+            </Col>
+            <Col lg="3" md-6>
+              <Card style={{ width: "18rem" }}>
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+              
+            </Col>
+            
+            
+          </Row>
+        </Container>
+      </Container>
+
+      <Footer />
     </div>
-
-
-   
   );
 }
 
 export default App;
+
