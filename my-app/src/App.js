ba470209e864f47b4d1a6890140a45e51952f8f2
Rahul Jangir <rahuljangir4368@gmail.com>
2024-12-18 20:49:16 +0530

    learn how to add probs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,88 +1,38 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { blog } from "./Data/blog";
 import Footer from "./Footer";
 import Header from "./header";
 import {Button, Container, Row, Col,Card } from "react-bootstrap";
 
 function App() {
+  let headerinfo = {
+    name :"rahul",
+    email :"rahul@gmail.com",
+    phone :"9999999999",
+    address :"xyz street, pqr city"
+
+  }
   return (
     <div className="main">
-      <Header />
+      <Header headerinfo = {headerinfo} />
+    
+     
+
+
+
 
       <Container>
-        <Container>
-          <Row>
-            <Col className="col-12" text-center py-4>
-              
-              <h1>Welcome to WS </h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg="3" md-6>
-              <Card style={{ width: "18rem" }}>
-                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
+        <Row>
+          {blog.map((v,i)=>{
+            return(
+              <ProductItem pitem = {v} key={i}/>
 
-              
-            </Col>
-
-            <Col lg="3" md-6>
-              <Card style={{ width: "18rem" }}>
-                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-
-              
-            </Col>
-            <Col lg="3" md-6>
-              <Card style={{ width: "18rem" }}>
-                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-
-              
-            </Col>
-            <Col lg="3" md-6>
-              <Card style={{ width: "18rem" }}>
-                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-
-              
-            </Col>
-            
-            
-          </Row>
-        </Container>
+            )
+          })}
+       
+        
+        </Row>
       </Container>
 
       <Footer />
@@ -92,3 +42,23 @@ function App() {
 
 export default App;
 
+function ProductItem({pitems}){
+  return (
+    <Col lg="3" md = "6">
+       <Card style={{ width: "18rem" }}>
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                  <Card.Title> {pitems.title} </Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+
+
+    </Col>
+  )
+}
--- my-app/src/header.jsx
+++ my-app/src/header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-export default function Header() {
+
+export default function Header(probs) {
+  console.log(probs.headerinfo.email) // will log the props object when the component is rendered
   return (
     <div>
       hi this is my first componet
